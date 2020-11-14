eb00add7cbd2ffc8d8ff2856fe9297c0d4583e82
alexandrYun <alexandryun@mail.ru>
2020-11-15 00:32:41 +0300

    vue_calculator

---
+++ src/components/Calc.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div><input type="text" v-model="x" /></div>
+        <div><input type="text" v-model="y" /></div>
+        <div><span v-text="result"></span></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            x: 0,
+            y: 0,
+            lastResult: 10,
+        }  
+    },
+    computed: {
+        result: function() {
+            let x = +this.x;
+            // if(Number.isNaN(x)) return this.lastResult;
+            let y = +this.y;
+            // if(Number.isNaN(y)) return this.lastResult;
+            // this.lastResult = x + y;
+            let res = x + y;
+            return `Результат сложения ${res}`
+        }
+    }
+}
+</script>
---
+++ src/components/Time.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <span v-text="hourtime"></span>
+        <span v-text="hours"></span>
+        <span v-text="minutes"></span>
+        <span v-text="seconds"></span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        }  
+    },
+    mounted() {
+        this.$options.timer = window.setTimeout(this.updateDateTime, 1000);
+    },
+    methods: {
+            updateDateTime() {
+                const now = new Date();
+                this.hours = now.getHours();
+                this.minutes = now.getMinutes();
+                this.seconds =  now.getSeconds();
+                this.$options.timer = window.setTimeout(this.updateDateTime, 1000);
+        }
+    },
+}
+</script>
